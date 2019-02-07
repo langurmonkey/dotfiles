6ca52a34996cd7ecd961c376e2082811b66ee1af
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2019-02-07 15:42:48 +0100

    Add ag and entr

--- bootstrap-arch.sh
+++ bootstrap-arch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Deploys the environment in arch and derivatives
-sudo pacman --needed --noconfirm -S git zsh vim ranger slock qutebrowser dmenu keychain udiskie compton playerctl rxvt-unicode pasystray blueman i3status feh
+sudo pacman --needed --noconfirm -S git zsh vim ranger slock qutebrowser dmenu keychain udiskie compton playerctl rxvt-unicode pasystray blueman i3status feh the_silver_searcher entr
 sudo yay --noconfirm -S mons i3-gaps pa-applet-git terminus-font-ttf
