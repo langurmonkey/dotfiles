548d0078ec9fe448bb48feee10d6d715f52ef13e
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2019-02-07 16:01:19 +0100

    Add mupdf

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Deploys the environment in arch and derivatives
-sudo apt-get update && sudo apt-get install -y git zsh vim ranger slock qutebrowser dmenu keychain udiskie feh xfonts-terminus silversearcher-ag entr
+sudo apt-get update && sudo apt-get install -y git zsh vim ranger slock qutebrowser dmenu keychain udiskie feh xfonts-terminus silversearcher-ag entr mupdf mupdf-tools
