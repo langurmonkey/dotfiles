a08489d31768ee319a0d85979aabeafee4fc2a21
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2019-02-28 10:09:37 +0100

    Improved cpu and mem modules. Fixed dpi issues in calendar

--- polybar/scripts/cpu.sh
+++ polybar/scripts/cpu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo " $(grep 'cpu ' /proc/stat | awk '{cpu_usage=($2+$4)*100/($2+$4+$5)} END {printf "%0.2f%", cpu_usage}')"
+echo " $(grep 'cpu ' /proc/stat | awk '{cpu_usage=($2+$4)*100/($2+$4+$5)} END {printf "%0.2f%", cpu_usage}')   $(sensors | grep temp1 | head -1 | awk '{print $2}')"
