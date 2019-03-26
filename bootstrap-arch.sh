696f5946ebf017f893e0f28bc968d9892bb324ff
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2019-03-26 14:50:36 +0100

    add cava to bootstrap arch

--- bootstrap-arch.sh
+++ bootstrap-arch.sh
@@ -2,4 +2,4 @@
 
 # Deploys the environment in arch and derivatives
 sudo pacman --needed --noconfirm -S git zsh neovim ranger slock qutebrowser dmenu keychain udiskie compton playerctl rxvt-unicode pasystray blueman i3status feh the_silver_searcher entr mupdf mupdf-tools dunst fzf fd asciinema
-yay --noconfirm -S mons i3-gaps pa-applet-git terminus-font-ttf
+yay --noconfirm -S mons i3-gaps pa-applet-git terminus-font-ttf cava
