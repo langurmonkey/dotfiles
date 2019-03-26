5595e99eef6e0075463db0e71f0fda206f5cb924
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2019-03-26 13:41:50 +0100

    asciinema to bootstrap

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Deploys the environment in arch and derivatives
-sudo apt-get update && sudo apt-get install -y git zsh neovim ranger slock qutebrowser dmenu keychain udiskie feh xfonts-terminus silversearcher-ag entr mupdf mupdf-tools dunst
+sudo apt update && sudo apt install -y git zsh neovim ranger slock qutebrowser dmenu keychain udiskie feh xfonts-terminus silversearcher-ag entr mupdf mupdf-tools dunst asciinema
