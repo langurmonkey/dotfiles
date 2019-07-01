98b136030769e49758dcd1b1e41955425d9d7044
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2019-07-01 14:41:46 +0200

    add wal link and all

--- bootstrap-arch.sh
+++ bootstrap-arch.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-# Deploys the environment in arch and derivatives
-sudo pacman --needed --noconfirm -S git zsh neovim ranger slock qutebrowser dmenu keychain udiskie compton playerctl rxvt-unicode pasystray blueman i3status feh the_silver_searcher entr mupdf mupdf-tools dunst fzf fd mpd ncmpcpp
-yay --noconfirm -S mons i3-gaps pa-applet-git terminus-font-ttf cava polybar ttf-font-awesome-4
+# Create XDG directories
+sudo pacman --needed --noconfirm -S xdg-user-dirs
+xdg-user-dirs-update
+
+# Deploy environment
+sudo pacman --needed --noconfirm -S git zsh neovim ranger slock qutebrowser dmenu keychain udiskie compton playerctl rxvt-unicode pasystray blueman i3status feh the_silver_searcher entr mupdf mupdf-tools dunst fzf fd mpd ncmpcpp w3m
+yay --noconfirm -S mons i3-gaps pa-applet-git terminus-font-ttf cava polybar ttf-font-awesome-4 python-pywal
+
+# wal
+ln -s $(which wal) ~/.local/bin/wal
