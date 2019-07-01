6aad1459197ff82797dda206789a3e434340a882
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2019-07-01 15:11:33 +0200

    add libcaca and sxiv to bootstrap

--- bootstrap-arch.sh
+++ bootstrap-arch.sh
@@ -5,7 +5,7 @@ sudo pacman --needed --noconfirm -S xdg-user-dirs
 xdg-user-dirs-update
 
 # Deploy environment
-sudo pacman --needed --noconfirm -S git zsh neovim ranger slock qutebrowser dmenu keychain udiskie compton playerctl rxvt-unicode pasystray blueman i3status feh the_silver_searcher entr mupdf mupdf-tools dunst fzf fd mpd ncmpcpp w3m
+sudo pacman --needed --noconfirm -S git zsh neovim ranger slock qutebrowser dmenu keychain udiskie compton playerctl rxvt-unicode pasystray blueman i3status feh the_silver_searcher entr mupdf mupdf-tools dunst fzf fd mpd ncmpcpp w3m libcaca sxiv
 yay --noconfirm -S mons i3-gaps pa-applet-git terminus-font-ttf cava polybar ttf-font-awesome-4 python-pywal
 
 # wal
