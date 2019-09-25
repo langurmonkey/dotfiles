d1764bdfd8a575dd110a175f7f7d53d62eea057f
langurmonkey <me@tonisagrista.com>
2019-09-25 11:57:37 +0200

    Many changes to polybar

--- polybar/scripts/memory.sh
+++ polybar/scripts/memory.sh
@@ -5,6 +5,6 @@ case "$1" in
         notify-send "Memory (%)" "$(ps axch -o cmd:10,pmem k -pmem | head | awk '$0=$0"%"' )"
         ;;
     *)
-        echo " $(free -h --si | awk '/^Mem:/ {print $3 "/" $2}')"
+        echo "$(free -h --si | awk '/^Mem:/ {print $3 "/" $2}')"
         ;;
 esac
--- polybar/scripts/updates-arch.sh
+++ polybar/scripts/updates-arch.sh
@@ -5,4 +5,4 @@ arch_updates=$(checkupdates 2>&1)
 aur_updates=$(yay --aur -Qu 2>/dev/null | wc -l)
 arch_updates=$(echo "$arch_updates" | wc -l)
 updates=$((arch_updates + aur_updates))
-echo " $updates"
+echo "$updates"
