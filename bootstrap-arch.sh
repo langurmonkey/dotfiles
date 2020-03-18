844f6bed16903e39fde1009bac5b4c5e19326c84
langurmonkey <me@tonisagrista.com>
2020-03-18 14:16:25 +0100

    vim link

--- bootstrap-arch.sh
+++ bootstrap-arch.sh
@@ -8,5 +8,8 @@ xdg-user-dirs-update
 sudo pacman --needed --noconfirm -S git zsh neovim ranger slock qutebrowser dmenu keychain udiskie compton playerctl rxvt-unicode pasystray blueman i3status feh the_silver_searcher entr mupdf mupdf-tools dunst fzf fd mpd ncmpcpp w3m libcaca sxiv nnn vlock
 yay --noconfirm -S mons i3-gaps pa-applet-git terminus-font-ttf cava polybar ttf-font-awesome-4 python-pywal advcp
 
+# Link vim to nvim
+sudo ln -s /usr/bin/nvim /usr/bin/vim
+
 # wal
 ln -s $(which wal) ~/.local/bin/wal
