275b3465f2bc5dd0a9d855af23d127ff4b769fc9
langurmonkey <me@tonisagrista.com>
2020-03-20 08:17:26 +0100

    add wmfocus, key binding to reload wal

--- bootstrap-arch.sh
+++ bootstrap-arch.sh
@@ -6,7 +6,7 @@ xdg-user-dirs-update
 
 # Deploy environment
 sudo pacman --needed --noconfirm -S git zsh neovim ranger slock qutebrowser dmenu keychain udiskie compton playerctl rxvt-unicode pasystray blueman i3status feh the_silver_searcher entr mupdf mupdf-tools dunst fzf fd mpd ncmpcpp w3m libcaca sxiv nnn vlock python-pywal
-yay --noconfirm -S mons i3-gaps pa-applet-git terminus-font-ttf cava polybar-git ttf-font-awesome-4 advcp
+yay --noconfirm -S mons i3-gaps pa-applet-git terminus-font-ttf cava polybar-git ttf-font-awesome-4 advcp wmfocus
 
 # Link vim to nvim
 [ ! -f /usr/bin/vim ] && sudo ln -s /usr/bin/nvim /usr/bin/vim
