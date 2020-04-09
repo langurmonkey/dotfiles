cb9fddd76a99e6aaf656248347d22fb8f4dc2dbd
langurmonkey <me@tonisagrista.com>
2020-04-09 08:34:14 +0200

    Reject unsecure js in qtb by default, add neovim dictionary additions

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -702,7 +702,7 @@ c.content.pdfjs = True
 ##   - true
 ##   - false
 ##   - ask
-# c.content.ssl_strict = 'ask'
+c.content.ssl_strict = True
 
 ## List of user stylesheet filenames to use.
 ## Type: List of File, or File
