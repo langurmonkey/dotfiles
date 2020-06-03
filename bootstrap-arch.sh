af743cc9e39fb2a7db9d469a03effa6d9759dd63
langurmonkey <me@tonisagrista.com>
2020-06-03 19:58:13 +0000

    More

--- bootstrap-arch.sh
+++ bootstrap-arch.sh
@@ -5,7 +5,7 @@ sudo pacman --needed --noconfirm -S xdg-user-dirs
 xdg-user-dirs-update
 
 # Deploy environment
-sudo pacman --needed --noconfirm -S git zsh neovim ranger slock qutebrowser dmenu keychain udiskie compton playerctl pasystray blueman i3status feh the_silver_searcher entr mupdf mupdf-tools dunst fzf fd mpd ncmpcpp w3m libcaca sxiv nnn vlock python-pywal numlockx ttf-nerd-fonts-symbols xdotool yad termite tor terminus-font-ttf lxappearance mpv udiskie
+sudo pacman --needed --noconfirm -S git zsh neovim ranger slock qutebrowser dmenu keychain udiskie compton playerctl pasystray blueman i3status feh the_silver_searcher entr mupdf mupdf-tools dunst fzf fd mpd ncmpcpp w3m libcaca sxiv nnn vlock python-pywal numlockx ttf-nerd-fonts-symbols xdotool yad termite tor terminus-font-ttf lxappearance mpv udiskie xorg-xinput ncdu thunderbird adapta-gtk-theme arc-gtk-theme
 yay --noconfirm -S mons i3-gaps pa-applet-git terminus-font-ttf cava polybar-git ttf-font-awesome-4 advcp wmfocus fzf polybar-git ly-git
 
 # Enable services
