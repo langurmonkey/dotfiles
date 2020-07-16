6a190f3f4f63e5beb3a3aec7aa9f8b2f7c35a38e
langurmonkey <me@tonisagrista.com>
2020-07-16 12:23:59 +0200

    feat: more nnn

---
+++ nnn/conf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export NNN_OPTS='cE'
+export NNN_OPENER=$HOME/.config/nnn/plugins/nuke
+export NNN_BMS='D:~/Documents;P:~/Projects'
+export NNN_COLORS='12345678'
+export NNN_ARCHIVE='\\.(7z|bz2|gz|tar|tgz|zip)$'
