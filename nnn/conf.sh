b86e8bc68da74fbb0c2ad41f5d1e4f27a2b07889
langurmonkey <me@tonisagrista.com>
2020-07-16 17:15:22 +0200

    feat: add plugins to nnn

--- nnn/conf.sh
+++ nnn/conf.sh
@@ -4,3 +4,6 @@ export NNN_OPENER=$HOME/.config/nnn/plugins/nuke
 export NNN_BMS='D:~/Documents;P:~/Projects'
 export NNN_COLORS='12345678'
 export NNN_ARCHIVE='\\.(7z|bz2|gz|tar|tgz|zip)$'
+export NNN_PLUG='f:finder;o:fzopen;p:preview-tui;d:diffs;v:imgview;m:mediainf'
+export NNN_FIFO='/tmp/nnn.fifo'
+
