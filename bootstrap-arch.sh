3906872b8b64ba77bd7cc4f591bd932d99da558d
langurmonkey <me@tonisagrista.com>
2020-10-12 11:51:42 +0200

    fix: add more utils

--- bootstrap-arch.sh
+++ bootstrap-arch.sh
@@ -5,7 +5,7 @@ sudo pacman --needed --noconfirm -S xdg-user-dirs
 xdg-user-dirs-update
 
 # Deploy environment
-yay --noconfirm -S git zsh neovim ranger slock qutebrowser dmenu keychain udiskie compton playerctl pasystray blueman i3status feh the_silver_searcher entr mupdf mupdf-tools dunst fzf fd mpd ncmpcpp w3m libcaca sxiv nnn vlock numlockx xdotool yad termite tor lxappearance mpv udiskie xorg-xinput ncdu thunderbird adapta-gtk-theme arc-gtk-theme net-tools mpv xorg-xrdb neofetch rsync openvpn networkmanager-openvpn man xdo mons i3-gaps pa-applet-git cava advcp wmfocus polybar-git ly-git i3lock-fancy scrot xorg-xdpyinfo ytop bat xorg-xrandr ncmpcpp pywal-git keychain gucharmap
+yay --noconfirm -S git zsh neovim ranger slock qutebrowser dmenu keychain udiskie compton playerctl pasystray blueman i3status feh the_silver_searcher entr mupdf mupdf-tools dunst fzf fd mpd ncmpcpp w3m libcaca sxiv nnn vlock numlockx xdotool yad termite tor lxappearance mpv udiskie xorg-xinput ncdu thunderbird adapta-gtk-theme arc-gtk-theme net-tools mpv xorg-xrdb neofetch rsync openvpn networkmanager-openvpn man xdo mons i3-gaps pa-applet-git cava advcp wmfocus polybar-git ly-git i3lock-fancy scrot xorg-xdpyinfo ytop bat xorg-xrandr ncmpcpp pywal-git keychain gucharmap pulseaudio pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-alsa pavucontrol
 
 # Enable services
 sudo systemctl enable tor.service
