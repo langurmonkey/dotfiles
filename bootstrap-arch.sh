226d21839944ac313f2b718c4f43df7585aa2e7b
langurmonkey <me@tonisagrista.com>
2020-10-12 10:39:42 +0200

    feat: missing packages

--- bootstrap-arch.sh
+++ bootstrap-arch.sh
@@ -6,7 +6,7 @@ xdg-user-dirs-update
 
 # Deploy environment
 sudo pacman --needed --noconfirm -S git zsh neovim ranger slock qutebrowser dmenu keychain udiskie compton playerctl pasystray blueman i3status feh the_silver_searcher entr mupdf mupdf-tools dunst fzf fd mpd ncmpcpp w3m libcaca sxiv nnn vlock python-pywal numlockx ttf-nerd-fonts-symbols xdotool yad termite tor terminus-font-ttf terminus-font-otb lxappearance mpv udiskie xorg-xinput ncdu thunderbird adapta-gtk-theme arc-gtk-theme net-tools mpv xorg-xrdb neofetch rsynch openvpn networkmanager-openvpn man xdo
-yay --noconfirm -S mons i3-gaps pa-applet-git cava polybar-git ttf-font-awesome-4 advcp wmfocus fzf polybar-git ly-git i3lock-fancy scrot mons xorg-xpdyinfo ytop bat
+yay --noconfirm -S mons i3-gaps pa-applet-git cava polybar-git ttf-font-awesome-4 advcp wmfocus fzf polybar-git ly-git i3lock-fancy scrot mons xorg-xdpyinfo ytop bat xorg-xrandr ncmpcpp pywal-git
 
 # Enable services
 sudo systemctl enable tor.service
