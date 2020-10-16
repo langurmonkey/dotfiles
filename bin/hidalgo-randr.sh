079bf630ca1cdd4df7d2710b8afc0f8c931f86d1
langurmonkey <me@tonisagrista.com>
2020-10-16 15:27:45 +0200

    fix: hopefully, fix hidalgo-randr

--- bin/hidalgo-randr.sh
+++ bin/hidalgo-randr.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-xrandr  --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate left --scale 1.5x1.5 --output DP-2 --primary --mode 3840x2160 --pos 1920x0 --rotate normal
-#xrandr --output DP-2 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate normal --scale 1x1
+#xrandr  --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate left --scale 1.5x1.5 --output DP-2 --primary --mode 3840x2160 --pos 1920x0 --rotate normal
+
+xrandr --dpi 190 --output DP-2 --primary --mode 3840x2160 --pos 2160x600 --output DVI-D-0 --scale 2x2 --mode 1920x1080 --rotate left 
