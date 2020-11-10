68eafc68303408bb485386a5d8934c31f9c2f0b8
langurmonkey <me@tonisagrista.com>
2020-11-10 09:49:42 +0100

    feat: update nnn plugins

--- nnn/conf.sh
+++ nnn/conf.sh
@@ -7,3 +7,31 @@ export NNN_ARCHIVE='\\.(7z|bz2|gz|tar|tgz|zip)$'
 export NNN_PLUG='f:finder;o:fzopen;p:preview-tui;d:diffs;v:imgview;m:mediainf'
 export NNN_FIFO='/tmp/nnn.fifo'
 
+n ()
+{
+    # Block nesting of nnn in subshells
+    if [ -n $NNNLVL ] && [ "${NNNLVL:-0}" -ge 1 ]; then
+        echo "nnn is already running"
+        return
+    fi
+
+    # The default behaviour is to cd on quit (nnn checks if NNN_TMPFILE is set)
+    # To cd on quit only on ^G, remove the "export" as in:
+    #     NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+    # NOTE: NNN_TMPFILE is fixed, should not be modified
+    export NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+
+    # Unmask ^Q (, ^V etc.) (if required, see `stty -a`) to Quit nnn
+    # stty start undef
+    # stty stop undef
+    # stty lwrap undef
+    # stty lnext undef
+
+    nnn "$@"
+
+    if [ -f "$NNN_TMPFILE" ]; then
+            . "$NNN_TMPFILE"
+            rm -f "$NNN_TMPFILE" > /dev/null
+    fi
+}
+
