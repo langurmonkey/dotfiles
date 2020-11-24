ad1f9de327e52acfe76606394dd7fc61f5774617
langurmonkey <me@tonisagrista.com>
2020-11-24 08:51:55 +0100

    feat: fix hidlago-randr, add thefuck to env, fix i3 startup sequence

--- bin/hidalgo-randr.sh
+++ bin/hidalgo-randr.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 #xrandr  --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate left --scale 1.5x1.5 --output DP-2 --primary --mode 3840x2160 --pos 1920x0 --rotate normal
 
-xrandr --dpi 190 \
+file=/tmp/xrandr-out.log
+
+echo "------ XRANDR STATUS ------" >> $file
+sleep 2
+xrandr -q >> $file
+echo "------ XRANDR COMMAND OUTPUT BELOW ------" >> $file
+xrandr --verbose --dpi 190 \
     --output DVI-D-0 --scale 2x2 --mode 1920x1080 --pos 0x0 --rotate normal \
-    --output DP-2 --primary --mode 3840x2160 --pos 3840x0 --rotate normal 
+    --output DP-2 --primary --mode 3840x2160 --pos 3900x0 --rotate normal \
+    >> $file 2>&1
+echo "------ DONE ------" >> $file
+echo ""
