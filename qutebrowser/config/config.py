406edbfb95287ab7e7f01bf22feb5a4100c7e051
langurmonkey <me@tonisagrista.com>
2020-12-02 16:08:48 +0100

    feat: add readability-js support, remove password_fill script (already with distro)

--- qutebrowser/config/config.py
+++ qutebrowser/config/config.py
@@ -46,7 +46,7 @@ c.aliases.update({'drive': 'open -t https://heibox.uni-heidelberg.de/accounts/lo
 c.aliases.update({'hb': 'open -t https://heibox.uni-heidelberg.de/accounts/login/?next=/'}) 
 c.aliases.update({'nas': 'open -t http://192.168.0.26:8080'}) 
 c.aliases.update({'nasout': 'open -t qlink.to/jumpinglangur'}) 
-c.aliases.update({"clean" : "spawn --userscript readability"})
+c.aliases.update({"clean" : "spawn --userscript readability-js"})
 
 
 ## Width (in pixels) of the scrollbar in the completion window.
