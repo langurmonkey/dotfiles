cbbdfc7414eb2defe02508c01ccc7e82d1c223ea
langurmonkey <me@tonisagrista.com>
2021-06-07 12:34:44 +0200

    feat: update adblock lists

--- qutebrowser/config/config.py
+++ qutebrowser/config/config.py
@@ -149,7 +149,7 @@ c.content.user_stylesheets = [ "scrollbar.css" ]
 ##
 c.content.blocking.enabled = True
 c.content.blocking.method = 'both'
-c.content.blocking.adblock.lists = ["https://easylist.to/easylist/easylist.txt", "https://easylist.to/easylist/easyprivacy.txt", "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt"]
+c.content.blocking.adblock.lists = ['https://easylist.to/easylist/easylist.txt', 'https://easylist.to/easylist/easyprivacy.txt', 'https://easylist-downloads.adblockplus.org/easylistdutch.txt', 'https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt', 'https://www.i-dont-care-about-cookies.eu/abp/', 'https://secure.fanboy.co.nz/fanboy-cookiemonster.txt']
 
 ## Editor (and arguments) to use for the `open-editor` command. The
 ## following placeholders are defined: * `{file}`: Filename of the file
