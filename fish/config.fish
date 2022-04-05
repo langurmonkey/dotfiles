b9fe632e7f3a5b48ff63089b34078a52122cef2e
langurmonkey <me@tonisagrista.com>
2022-04-05 09:37:34 +0200

    feat: remove spacefish (or whatever), use starship. Add .env_bash

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,8 @@
+# Source aliases
 . ~/.config/fish/aliases.fish
+
+# Starship prompt
+starship init fish | source
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
