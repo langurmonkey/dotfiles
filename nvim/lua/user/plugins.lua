b323ae921cd3e93b64f4742704f3f8740648550c
langurmonkey <me@tonisagrista.com>
2022-07-18 13:19:54 +0200

    feat: add symbols-outline plugin to nvim, access with spc+o.

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -59,6 +59,7 @@ return packer.startup(function(use)
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
   use "folke/which-key.nvim"
   use "petertriho/nvim-scrollbar" -- A scrollbar for neovim
+  use "simrat39/symbols-outline.nvim" -- Outline view using LSP
 
   -- Smooth scrolling for C-f and C-b
   use "psliwka/vim-smoothie"
