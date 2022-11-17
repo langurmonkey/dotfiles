94e49e67e814e0fc6344980131df4e05030746b8
langurmonkey <me@tonisagrista.com>
2022-11-17 15:30:44 +0100

    feat: replace starship shell install with pacman version.

--- bootstrap-arch.sh
+++ bootstrap-arch.sh
@@ -18,13 +18,10 @@ else
 fi
 
 # Deploy environment
-yay --noconfirm -S zsh neovim neovide code-minimap ranger slock qutebrowser python-adblock dmenu rofi rofi-calc rofi-pass rofi-power-menu keychain udiskie picom-ibhagwan-git playerctl pasystray blueman feh ripgrep entr mupdf mupdf-tools dunst fzf fd mpd mpdris2 mpc ncmpcpp w3m libcaca imv nnn lf vlock numlockx xdotool yad kitty tor lxappearance udiskie xorg-xinput ncdu thunderbird adapta-gtk-theme arc-gtk-theme net-tools mpv mpv-mpris xorg-xrdb neofetch rsync openvpn networkmanager-openvpn network-manager-applet man xdo mons i3-gaps pa-applet-git cli-visualizer advcp wmfocus ly i3lock-fancy maim flameshot xorg-xdpyinfo btop bat xorg-xrandr arandr pywal keychain gucharmap pipewire pipewire-pulse pipewire-alsa pavucontrol pulsemixer pamixer youtube-dl gst-plugins-{base,good,bad,ugly} gst-libav figlet exa tokei procs zsh-syntax-highlighting zsh-autosuggestions zsh-history-substring-search i3blocks i3blocks-contrib kbdd sysstat xorg-xbacklight brightnessctl hsetroot cowsay lolcat fortune-mod rpg-cli git-delta xob python-watchdog python-pulsectl nsxiv autotiling mailcap run-mailcap viu zathura chafa ueberzug noto-fonts-emoji
+yay --noconfirm -S zsh neovim neovide code-minimap ranger slock qutebrowser python-adblock dmenu rofi rofi-calc rofi-pass rofi-power-menu keychain udiskie picom-ibhagwan-git playerctl pasystray blueman feh ripgrep entr mupdf mupdf-tools dunst fzf fd mpd mpdris2 mpc ncmpcpp w3m libcaca imv nnn lf vlock numlockx xdotool yad kitty tor lxappearance udiskie xorg-xinput ncdu thunderbird adapta-gtk-theme arc-gtk-theme net-tools mpv mpv-mpris xorg-xrdb neofetch rsync openvpn networkmanager-openvpn network-manager-applet man xdo mons i3-gaps pa-applet-git cli-visualizer advcp wmfocus ly i3lock-fancy maim flameshot xorg-xdpyinfo btop bat xorg-xrandr arandr pywal keychain gucharmap pipewire pipewire-pulse pipewire-alsa pavucontrol pulsemixer pamixer youtube-dl gst-plugins-{base,good,bad,ugly} gst-libav figlet exa tokei procs zsh-syntax-highlighting zsh-autosuggestions zsh-history-substring-search i3blocks i3blocks-contrib kbdd sysstat xorg-xbacklight brightnessctl hsetroot cowsay lolcat fortune-mod rpg-cli git-delta xob python-watchdog python-pulsectl nsxiv autotiling mailcap run-mailcap viu zathura chafa ueberzug noto-fonts-emoji starship
 
 # Enable services
 # sudo systemctl enable tor.service
-
-# Install starship
-curl -fsSL https://starship.rs/install.sh | sh
 
 # Link vim to nvim
 [ ! -f /usr/bin/vim ] && sudo ln -s /usr/bin/nvim /usr/bin/vim
