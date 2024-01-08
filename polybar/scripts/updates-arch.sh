ca7cca737fd09df17a8649e05f6460e671e2a1e5
langurmonkey <me@tonisagrista.com>
2024-01-08 15:32:37 +0100

    fix: leftovers in replacing yay with paru.

--- polybar/scripts/updates-arch.sh
+++ polybar/scripts/updates-arch.sh
@@ -2,7 +2,7 @@
 
 # Arch updates includes ruby gem updates as well (little did you know, huh?)
 arch_updates=$(checkupdates 2>&1)
-aur_updates=$(yay --aur -Qu 2>/dev/null | wc -l)
+aur_updates=$(paru --aur -Qu 2>/dev/null | wc -l)
 arch_updates=$(echo "$arch_updates" | wc -l)
 updates=$((arch_updates + aur_updates))
 echo "$updates"
