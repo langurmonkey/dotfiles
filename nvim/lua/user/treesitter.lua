2c5f71975ac8902ec84a452e0dbab3af1be78d40
langurmonkey <me@tonisagrista.com>
2024-04-19 14:43:49 +0200

    none: ....

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,9 +1,4 @@
-local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-if not status_ok then
-  return
-end
-
-configs.setup {
+require'nvim-treesitter.configs'.setup {
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
@@ -16,10 +11,4 @@ configs.setup {
     additional_vim_regex_highlighting = false,
   },
   indent = { enable = true },
-}
-require('ts_context_commentstring').setup {
-  enable_autocmd = false,
-  languages = {
-    typescript = '// %s',
-  },
 }
