4553952fdf3bbf4971892323392c03e6d0f260ae
langurmonkey <me@tonisagrista.com>
2024-08-20 17:32:42 +0200

    fix: cowsay location.

--- env/env_fun.sh
+++ env/env_fun.sh
@@ -5,9 +5,9 @@ command_exists () {
 }
 # Let's hear the cow say...
 if command_exists fortune && command_exists cowsay && command_exists lolcat; then
-    fortune | cowsay -f $(\ls /usr/share/cows/*.cow|shuf -n 1) | lolcat
+    fortune | cowsay -f $(\ls /usr/share/cowsay/cows/*.cow|shuf -n 1) | lolcat
 elif command_exists fortune && command_exists cowsay && ! command_exists lolcat; then
-    fortune | cowsay -f $(\ls /usr/share/cows/*.cow|shuf -n 1)
+    fortune | cowsay -f $(\ls /usr/share/cowsay/cows/*.cow|shuf -n 1)
 elif command_exists fortune && ! command_exists cowsay && command_exists lolcat; then
     fortune | lolcat
 elif command_exists fortune; then
