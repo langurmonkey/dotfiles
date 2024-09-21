e7fb52aa9cc7439ee3875e898dbee6cf27843c88
langurmonkey <me@tonisagrista.com>
2024-09-21 20:15:15 +0200

    feat: restore tabs in yazi.

--- yazi/init.lua
+++ yazi/init.lua
@@ -16,16 +16,12 @@ function Header:host()
 	end
 	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("green")
 end
-function Header:render(area)
-	self.area = area
-
-	local right = ui.Line { self:count(), self:tabs() }
-    local left = ui.Line { self:host(), self:cwd(math.max(0, area.w - right:width())) }
-	return {
-		ui.Paragraph(area, { left }),
-		ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
-	}
-end
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ui.Line {}
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
 
 -- Enable copy/paste between instances.
 require("session"):setup {
