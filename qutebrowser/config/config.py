5fc07b812d0af4e3c4dc7d5f2cff5e2622cdd350
langurmonkey <me@tonisagrista.com>
2025-03-31 20:05:22 +0200

    feat: qwant as default search engine in qb.

--- qutebrowser/config/config.py
+++ qutebrowser/config/config.py
@@ -270,10 +270,10 @@ c.url.default_page = 'file://' + home + '/.dotfiles/startpage/index.html'
 ## used by prepending the search engine name to the search term, e.g.
 ## `:open google qutebrowser`.
 ## Type: Dict
-# c.url.searchengines = {'DEFAULT': 'https://www.qwant.com/?l=en&q={}&t=web'}
+c.url.searchengines = {'DEFAULT': 'https://www.qwant.com/?l=en&q={}&t=web'}
 #c.url.searchengines = {'DEFAULT': 'https://priv.au/search?q={}'}
 #c.url.searchengines = {'DEFAULT': 'https://search.brave.com/search?q={}'}
-c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}'}
+# c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}'}
 #c.url.searchengines = {'DEFAULT': 'http://localhost:8888?q={}'}
 c.url.searchengines.update({'lt': 'https://leta.mullvad.net/?q={}'})
 c.url.searchengines.update({'sx': 'https://priv.au/search?q={}'})
