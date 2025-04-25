65565ba69f95b5bc94d9c5a07efc78c7d1270f43
langurmonkey <me@tonisagrista.com>
2025-04-25 12:16:21 +0200

    fix: hint usage.

--- qutebrowser/config/config.py
+++ qutebrowser/config/config.py
@@ -333,9 +333,9 @@ config.bind('<Ctrl-p>', 'set-cmd-text -s :open -p ')
 config.bind('<Ctrl-e>', 'open-editor', mode='insert')
 
 ## Youtube special bindings
-config.bind(',v', 'spawn mpv {hint-url}')
-config.bind('V', 'spawn mpv {hint-url}')
-config.bind('A', 'spawn mpv {hint-url} --no-video')
+config.bind(',v', 'hint links spawn mpv {hint-url}')
+config.bind('V', 'hint links spawn mpv {hint-url}')
+config.bind('A', 'hint links spawn mpv {hint-url} --no-video')
 config.bind(';V', 'spawn kitty yt-dlp --all-subs --output ~/temp/vid/yt/%(title)s.%(ext)s {url}')
 config.bind(';A', 'spawn kitty yt-dlp --extract-audio --audio-format mp3 --output ~/temp/audio/yt/%(title)s.%(ext)s {url}')
 
