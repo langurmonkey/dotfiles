69e6f0151173035be3e1739c8af237f19d36d428
langurmonkey <me@tonisagrista.com>
2026-04-04 10:52:36 +0200

    feat: SC2000 to float.

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fortune | cowsay
 end
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/tsagrista/.lmstudio/bin
+# End of LM Studio CLI section
+
