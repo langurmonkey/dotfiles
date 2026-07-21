48f69fcdf17a502f6fe1ae75889e4b69a472a393
langurmonkey <me@tonisagrista.com>
2026-07-21 07:53:19 +0200

    none: Adjust animations for new hyprland version.

--- hyprland/common/animations.lua
+++ hyprland/common/animations.lua
@@ -8,16 +8,16 @@ hl.config({
 -- Bezier curves
 hl.curve("easeInOutCubic", { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}    } })
 hl.curve("linear",         { type = "bezier", points = { {0, 0},       {1, 1}       } })
-hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
+hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 0.5}   } })
 hl.curve("overshoot",      { type = "bezier", points = { {0.5, 0.9},   {0.1, 1.1}   } })
 
 -- Springs
 hl.curve("bouncy",         { type = "spring", mass = 3,   stiffness = 80, dampening = 10 })
 hl.curve("easy",           { type = "spring", mass = 2,   stiffness = 50, dampening = 10 })
-hl.curve("spring_ws",      { type = "spring", mass = 1.2, stiffness = 30, dampening = 8  })
-hl.curve("spring_win",     { type = "spring", mass = 1,   stiffness = 30, dampening = 8 })
+hl.curve("spring_ws",      { type = "spring", mass = 0.8, stiffness = 40, dampening = 8  })
+hl.curve("spring_win",     { type = "spring", mass = 0.9,   stiffness = 130, dampening = 12 })
 
 
-hl.animation({ leaf = "global",      enabled = true, speed = 5,   bezier = "quick" })
-hl.animation({ leaf = "workspaces",  enabled = true, speed = 7,   spring = "spring_ws",  style="slide" })
+hl.animation({ leaf = "global",      enabled = true, speed = 1,   bezier = "quick" })
+hl.animation({ leaf = "workspaces",  enabled = true, speed = 4,   spring = "spring_ws",  style="slide" })
 hl.animation({ leaf = "windows",     enabled = true, speed = 10,  spring = "spring_win", style = "slide" })
